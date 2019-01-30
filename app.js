dac05ae342e866871a926af3e459eeb5af5f6877
Vishesh-Dev <developer.vishesh@gmail.com>
2019-01-30 21:56:08 +0530

    set up start script and env port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app=express();
 const hbs = require('hbs');
-const port=3000;
+const port=process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname+'/views/partials')
 app.set('view engine','hbs');
